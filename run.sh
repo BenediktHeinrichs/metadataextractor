f77a5cef45e4330efb651bc1a8107101577e9623
Benedikt Heinrichs <49311227+BenediktHeinrichs@users.noreply.github.com>
2023-11-08 12:37:42 +0000

    Minify

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -f ./tika-server.jar ]
 then
-    wget -O ./tika-server.jar https://dlcdn.apache.org/tika/2.7.0/tika-server-standard-2.7.0.jar
+    wget -O ./tika-server.jar https://archive.apache.org/dist/tika/2.7.0/tika-server-standard-2.7.0.jar
 fi
 java -jar ./tika-server.jar &
 python ./server.py
