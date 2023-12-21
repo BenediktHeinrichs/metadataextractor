5cbdc715257ee35c84a727d098ac2a9c204dab3c
Benedikt Heinrichs <49311227+BenediktHeinrichs@users.noreply.github.com>
2023-12-21 13:05:40 +0100

    User based docker image (#7)
    * Push image with user
    
    * Update Dockerfile logic
    
    * Extract checkpoint retrieval

---
+++ getCheckpoints.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.cache/torch/hub/checkpoints
+wget -O ~/.cache/torch/hub/checkpoints/yolo_nas_l_coco.pth https://sghub.deci.ai/models/yolo_nas_l_coco.pth
+wget -O ~/.cache/torch/hub/checkpoints/mobilenet_v3_small-047dcff4.pth https://download.pytorch.org/models/mobilenet_v3_small-047dcff4.pth
