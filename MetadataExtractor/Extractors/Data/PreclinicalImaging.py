e5006f1cdd7897f5c80bb58a042c1a1a68e5c86f
catgonzftw1 <120124695+catgonzftw1@users.noreply.github.com>
2024-03-25 12:57:18 +0000

    attempted to add DICOM Extraction

---
+++ MetadataExtractor/Extractors/Data/PreclinicalImaging.py
@@ -0,0 +1,57 @@
+from .IDataExtract import IDataExtract
+from MetadataExtractor.Util import metadataCreation, metadataFormatter
+import pydicom
+from pydicom._version import __version_info__
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class DicomExtract(IDataExtract):
+    def extract(self, fileInfo):
+        log.info('Extracting metadata for Dicom file "' + fileInfo["file"] + '".')
+        text = ""
+
+        dicom_data = pydicom.dcmread(file)
+        
+        for elem in ds.iterall():
+            if elem.tag.is_private:
+                continue  # Skip private tags
+            tag_str = str(elem.tag)
+            value = elem.value
+            if isinstance(value, pydicom.valuerep.DSfloat):
+                value = float(value)
+            dicom_metadata[tag_str] = value
+
+        values = []
+        identifier = fileInfo["identifier"]
+
+        for attribute in attributes.keys():
+            objectValue = attributes[attribute]
+            values.append(
+                {
+                    "predicate": "mexattr:"
+                    + metadataFormatter.replaceForbiddenValues(attribute),
+                    "object": objectValue,
+                }
+            )
+
+        metadata = metadataCreation.addEntryToFileGraph(
+            fileInfo,
+            self._IExtract__config,
+            {
+                "additionalPrefixes": [
+                    "@prefix dcat: <http://www.w3.org/ns/dcat#>",
+                    "@prefix dcterms: <http://purl.org/dc/terms/>",
+                ],
+                "identifier": identifier,
+                "ontology": "mex",
+                "values": values,
+            },
+        )
+
+        return (text, metadata)
+
+    def registerMimeTypes(self):
+        self.mimeTypes["concrete"] = ["application/dcm"]
